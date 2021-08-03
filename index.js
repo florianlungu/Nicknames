7c079f83a8a3d316bd020f746b641033e116ea4c
Florian Lungu <34816324+florianlungu@users.noreply.github.com>
2021-08-02 20:30:02 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -27,21 +27,22 @@ app.command("/nickname", async ({ command, ack, say }) => {
   if (str.includes(",")) {
     var nameList = str.split(",");
     var changeFrom = nameList[0];
-    var changeTo = nameList[0];
+    var changeTo = nameList[1];
     changeFrom = changeFrom.trim();
     changeTo = changeTo.trim();
 
     // get user list
-    const result = await app.client.users.list({
+    var userList = await app.client.users.list({
       token: process.env.SLACK_BOT_TOKEN
     });
-
-    const obj = JSON.parse(JSON.stringify(result));
+    var userListObj = JSON.parse(JSON.stringify(userList));
+    
     let slackResponse = "Sorry I cannot find that member";
-    for (let i = 0; i < obj.members.length; i++) {
+    
+    for (let i = 0; i < userListObj.members.length; i++) {
 
-      var realName = obj.members[i].profile.real_name_normalized;
-      var displayName = obj.members[i].profile.display_name_normalized;
+      var realName = userListObj.members[i].profile.real_name_normalized;
+      var displayName = userListObj.members[i].profile.display_name_normalized;
       
       if (realName == changeFrom || displayName == changeFrom) {
         // update display name
@@ -49,7 +50,7 @@ app.command("/nickname", async ({ command, ack, say }) => {
           token: process.env.SLACK_USER_TOKEN,
           name: "display_name",
           value: changeTo,
-          user: obj.members[i].id
+          user: userListObj.members[i].id
         });
 
         if (actionResponse.ok == true) {
